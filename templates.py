4948d4dbeca9c81e4f267ac28037fb49fe3a8ed3
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-01-06 10:20:13 +0530

    doumentation

--- templates.py
+++ templates.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # diaplys home.html file stored in templates folder
     return render_template('home.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html') # diaplys index.html file stored in templates folder 
 
 
 if __name__ == '__main__':
