8faf58a6043030fc678163bf5e1edaf5e60e1829
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-01-06 10:36:25 +0530

    writing python code in html file

--- templates.py
+++ templates.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # diaplys home.html file stored in templates folder
-    return render_template('home.html',content='flask tutorials') # passing params to html file 
+    return render_template('home.html',content=['ML','DL','CV','Python','Flask']) # passing params to html file 
     # a place holder named content will catch the content passed ( in html file)
 
 @app.route('/index')
