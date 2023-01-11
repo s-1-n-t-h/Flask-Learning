bbb049e971a770c7a0d190042aed12058c162a6c
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-01-11 17:54:09 +0530

    flask app for session mainenance

--- flash alerts/app.py
+++ flash alerts/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, reuquest, session
+from flask import Flask, url_for, render_template, request, session, redirect
 from datetime import timedelta
 from flask import flash
 
@@ -14,4 +14,28 @@ def home():
 def login():
     if request.method == 'POST':
         session.permanent = True
-        
+        user = request.form['name']
+        session['user'] = user
+        return redirect(url_for('user'))
+    else:
+        if 'user' in session:
+            return redirect(url_for('user'))
+        return render_template('login.html')
+
+ 
+@app.route('/user')
+def user():
+    if 'user' in session:
+        user = session['user']
+        return f"You are logged in {user}!"
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    flash("You have been logged out!",'warning')
+    return redirect(url_for('login'))
+
+if __name__=='__main__':
+    app.run(debug=True)
